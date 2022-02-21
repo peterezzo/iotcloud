3dc43354dad35ae8379cd033f5bf237f2181d85e
Pete Ezzo <peter.ezzo@gmail.com>
2022-02-21 21:09:51 +0000

    annotate mqtt

--- lib/mqtt/mqtt.py
+++ lib/mqtt/mqtt.py
@@ -1,22 +1,32 @@
 """
 Python object to work with MQTT Broker (tested with Mosquitto)
-
-Author: Pete Ezzo <peter.ezzo@gmail.com>
 """
 
-import sys
 import time
-import paho.mqtt.client  # type: ignore
+from typing import Any, Callable
+from paho.mqtt.client import Client, MQTTMessage  # type: ignore
 
 
 class MQTT():
-    def __init__(self, host, port=1883, user=None, password=None, keepalive=60, client_id=None, connect=True):
-        self.client_id = client_id
+    def __init__(self,
+                 host: str,
+                 port: int = 1883,
+                 user: str = None,
+                 password: str = None,
+                 keepalive: int = 60,
+                 client_id: str = None,
+                 use_ssl: bool = False
+                 ) -> None:
+
         self.host = host
         self.port = port
         self.keepalive = keepalive
         self.client_id = client_id
-        self.client = paho.mqtt.client.Client(client_id)
+        self.client = Client(client_id)
+
+        if use_ssl:
+            self.client.tls_set()
+            self.client.tls_insecure_set(True)
 
         if user or password:
             self.client.username_pw_set(user, password)
@@ -25,38 +35,35 @@ class MQTT():
         self.client.on_connect = self.on_connect
         self.client.on_disconnect = self.on_disconnect
 
-        if connect:
-            self.client.connect(host, port, keepalive)
-
-    def on_message(self, mosq, obj, msg):
+    def on_message(self, client: Client, userdata: Any, msg: MQTTMessage) -> None:
         print(f'Msg: {msg.topic} {str(msg.qos)} {str(msg.payload)}')
         if msg.topic == 'Commands/ALL' and msg.payload == b'check-in':
-            self.pub('Notifications/check-in-reply', self.client_id or sys.argv[0], qos=1)
+            self.pub('Notifications/check-in-reply', self.client_id, qos=1)
 
-    def on_connect(self, mqttc, obj, flags, rc):
-        print(f'Connected to {mqttc._host}:{mqttc._port} code {rc}', flush=True)
+    def on_connect(self, client: Client, userdata: Any, flags: dict, rc: int) -> None:
+        print(f'Connected to {client._host}:{client._port} code {rc}')
         self.sub(None, 'Commands/ALL', qos=1)
-        self.pub('Notifications/startup', f'{self.client_id or sys.argv[0]} connect at {time.time()}', qos=1)
+        self.pub('Notifications/startup', f'{self.client_id} connect at {time.time()}', qos=1)
 
-    def on_disconnect(self, mqttc, obj, rc):
-        print(f'Disconnected from {mqttc._host}:{mqttc._port} code {rc}', flush=True)
+    def on_disconnect(self, client: Client, userdata: Any, rc: int) -> None:
+        print(f'Disconnected from {client._host}:{client._port} code {rc}')
 
-    def pub(self, topic, message, retain=False, qos=0):
+    def pub(self, topic: str, message: str, retain: bool = False, qos: int = 0) -> None:
         self.client.publish(topic, message, qos)
 
-    def sub(self, callback, topic, qos=0):
+    def sub(self, callback: Callable, topic: str, qos: int = 0) -> None:
         if callback:
             self.client.message_callback_add(topic, callback)
         self.client.subscribe(topic, qos)
 
-    def listen(self, blocking=False):
+    def listen(self, blocking: bool = False) -> None:
         """
         This is the main loop for a client waiting for messages
         """
-        # self.client.connect(host, port, keepalive)
+        self.client.connect(self.host, self.port, self.keepalive)
         if blocking:
             print('Starting blocking MQTT loop', flush=True)
             self.client.loop_forever()
         else:
-            print('Starting nonblocking MQTT loop', flush=True)
+            print('Starting nonblocking MQTT thread', flush=True)
             self.client.loop_start()
