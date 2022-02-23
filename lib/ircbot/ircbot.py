2347457046d11ac3452a4953f20b564ae37872bb
Pete Ezzo <peter.ezzo@gmail.com>
2022-02-23 04:24:23 +0000

    refactor mqtt-irc-bridge

---
+++ lib/ircbot/ircbot.py
@@ -0,0 +1,78 @@
+"""
+Python headless IRC Client
+"""
+
+import ssl
+from typing import Callable
+from irc.client import Reactor, DCCConnection, DCCConnectionError, Event, ServerConnection  # type: ignore
+from irc.connection import Factory  # type: ignore
+from jaraco.stream import buffer  # type: ignore
+
+ServerConnection.buffer_class = buffer.LenientDecodingLineBuffer
+
+
+class IRCBot():
+    def __init__(self,
+                 host: str,
+                 port: int,
+                 nick: str,
+                 nickpass: str = '',
+                 channels: list = None,
+                 use_ssl: bool = True
+                 ) -> None:
+        self.reactor = Reactor()
+        self.connection = self.reactor.server()
+
+        factory = Factory(wrapper=ssl.wrap_socket) if use_ssl else Factory()
+        self.connstring = {'server': host, 'port': port, 'nickname': nick, 'connect_factory': factory}
+
+        self.channels = channels
+        self.nickpass = nickpass
+        if self.nickpass:
+            print('Subbing for nickserv')
+            self.sub(self.nickserv_auth, 'privnotice')
+        else:
+            self.sub(self.join_channels, 'connect')
+
+    def start(self) -> None:
+        print('Connecting to IRC', flush=True)
+        self.connection.connect(**self.connstring)
+        print('Connected to IRC', flush=True)
+        self.reactor.process_forever(timeout=0.01)
+
+    def stop(self) -> None:
+        self.connection.disconnect('bye')
+        print('Cleanly exited')
+
+    def nickserv_auth(self, connection: ServerConnection, event: Event) -> None:
+        if event.source.nick == 'NickServ':
+            if 'IDENTIFY' in event.arguments[0]:
+                print('Identifying with NickServ')
+                connection.privmsg('NickServ', f'IDENTIFY {self.nickpass}')
+            elif 'Password accepted' in event.arguments[0]:
+                print('Identified with NickServ', flush=True)
+                self.join_channels()
+
+    def join_channels(self, *args, **kwargs) -> None:
+        for channel in self.channels:
+            print('JOINING', channel, flush=True)
+            self.connection.join(channel)
+
+    def sub(self, callback: Callable, event: str, priority: int = 0) -> None:
+        """Subscribe to an IRC event"""
+        self.reactor.add_global_handler(event, callback, priority)
+
+    def privmsg(self, target: str, msg: str) -> None:
+        self.connection.privmsg(target, msg)
+
+    def ctcp(self, type: str, target: str, msg: str) -> None:
+        self.connection.ctcp(type, target, msg)
+
+    def dcc(self) -> DCCConnection:
+        return self.reactor.dcc('raw')
+
+    def dcc_connect(self, peer_address: str, peer_port: int) -> DCCConnection | None:
+        try:
+            return self.reactor.dcc('raw').connect(peer_address, int(peer_port))
+        except DCCConnectionError as e:
+            print(f'Connection failed due to {e}')
--- mqtt-irc-bridge/mqtt-irc-bridge.py
+++ mqtt-irc-bridge/mqtt-irc-bridge.py
@@ -4,179 +4,26 @@ Python IRC Client controlled by MQTT Broker (tested with Mosquitto)
 Author: Pete Ezzo <peter.ezzo@gmail.com>
 """
 
-import collections
+import hashlib
 import json
 import os
 import pathlib
 import re
-import ssl
 import struct
-import time
-from typing import Any, Callable
-import irc.client  # type: ignore
-import paho.mqtt.client  # type: ignore
-from jaraco.stream import buffer  # type: ignore
-
-
-Transfer = collections.namedtuple('Transfer', ['dcc', 'file'])
-ServerConnection = irc.client.ServerConnection
-Event = irc.client.Event
-MQTTMessage = paho.mqtt.client.MQTTMessage
-Client = paho.mqtt.client.Client
-
-
-class IRC(irc.client.SimpleIRCClient):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__()
-        self.connargs = args
-        self.connkwargs = kwargs
-
-        self.debug_file = open('/data/unparsed.log', 'at')
-        self.dccfiles = {}
-        self.dccpeers = {}
-
-        self.watchlist = os.getenv('IRC_WATCHLIST').split(';')
-
-    def connect(self) -> None:
-        print('Connecting to IRC', flush=True)
-        if os.getenv('IRC_SSL'):
-            self.connkwargs['connect_factory'] = irc.connection.Factory(wrapper=ssl.wrap_socket)
-        self.connection.connect(*self.connargs, **self.connkwargs)
-
-    def join_channels(self) -> None:
-        channels = os.getenv('IRC_CHANNELS').split(';')
-        for channel in channels:
-            print('JOINING', channel, flush=True)
-            self.connection.join(channel)
-
-    def on_privmsg(self, connection: ServerConnection, event: Event) -> None:
-        print('PRIVMSG', event)
-
-    def on_pubmsg(self, connection: ServerConnection, event: Event) -> None:
-        if event.target in self.watchlist:
-            extract = re.match(r'.{4,16} +\d+x \[([^\]]+)\] (.*)', event.arguments[0])
-            if extract:
-                src = event.source.split('!')[0]
-                meta = extract.group(1)
-                name = extract.group(2)
-                mqtt.pub('IRC/watchlist', json.dumps({'src': src, 'meta': meta, 'name': name}))
-            else:
-                self.debug('PUBMSG', event)
-
-    def on_privnotice(self, connection: ServerConnection, event: Event) -> None:
-        if 'NickServ!NickServ@services' in event.source:
-            if 'IDENTIFY' in event.arguments[0]:
-                print('Identifying with NickServ')
-                connection.privmsg('NickServ', 'IDENTIFY ' + os.getenv('NICKSERV_PASS'))
-            elif 'Password accepted' in event.arguments[0]:
-                print('Identified')
-                self.join_channels()
-        elif 'MD5' in event.arguments[0]:
-            mqtt.pub('Notifications/irc', json.dumps({'notice': event.arguments[0]}))
-        else:
-            print('PRIVNOTICE', event)
-
-    def on_pubnotice(self, connection: ServerConnection, event: Event) -> None:
-        print('PUBNOTICE', event)
-
-    def on_ctcp(self, connection: ServerConnection, event: Event) -> None:
-        print('CTCP', event)
-        if event.arguments[0] == 'DCC':
-            command = event.arguments[1].split(maxsplit=1)[0]
-            peer_nick = event.source.split('!')[0]
-            if command == 'SEND':
-                self.handle_dcc_send(peer_nick, event.arguments[1])
-            elif command == 'ACCEPT':
-                self.handle_dcc_accept(peer_nick, event.arguments[1])
-
-    def on_dccmsg(self, connection: ServerConnection, event: Event) -> None:
-        if self.dccfiles.get(event.source):
-            data = event.arguments[0]
-            received_bytes = self.dccfiles[event.source].file.write(data)
-            self.dccfiles[event.source].dcc.send_bytes(received_bytes)
-        else:
-            print('DCC-MSG', event)
-
-    def on_dcc_disconnect(self, connection: ServerConnection, event: Event) -> None:
-        print('DCC-DISCONNECT', event)
-        if self.dccfiles.get(event.source):
-            _, file = self.dccfiles.pop(event.source)
-            print(f'Received file {file.filename} ({file.received_bytes}B out of {file.size}B).')
-            mqtt.pub('Notifications/irc', f'Transferred {file.received_bytes/file.size*100:0.2f}% of {file.filename}')
-            file.close()
-
-    def on_ping(self, connection: ServerConnection, event: Event) -> None:
-        print('PING', event)
-
-    def on_disconnect(self, connection: ServerConnection, event: Event) -> None:
-        self.debug_file.close()
-        for file in self.dccfiles:
-            file.close()
-
-    def debug(self, *msg) -> None:
-        print(*msg, file=self.debug_file, flush=True)
-
-    def handle_dcc_send(self, peer_nick: str, msg: str) -> None:
-        try:
-            # DCC SEND filename ip port size
-            _, filename, peer_address, peer_port, size = msg.split()
-        except ValueError:
-            print('CTCP DCC SEND unexpected format in request')
-            return
-
-        mqtt.pub('Notifications/irc', f'Incoming transfer of {filename}')
-        file = File(filename, int(size))
-        if file.received:
-            print(f'{file.filename} is already fetched.')
-            mqtt.pub('Notifications/irc', f'Not starting transfer of {filename}, already fetched')
-        else:
-            peer_address = irc.client.ip_numstr_to_quad(peer_address)
-            self.dccpeers[peer_nick] = peer_address
-            self.dccfiles[peer_address] = Transfer(self.dcc('raw'), file)
-
-            if file.received_bytes > 8:
-                print(f'{file.filename} could resume. Requesting.')
-                # DCC RESUME filename port position
-                # subtract 8 bytes à la mirc
-                self.connection.ctcp('DCC', peer_nick, f'RESUME {filename} {peer_port} {file.received_bytes - 8}')
-            else:
-                self._safe_dcc_connect(peer_address, peer_port)
-
-    def handle_dcc_accept(self, peer_nick: str, msg: str) -> None:
-        # DCC ACCEPT filename port position
-        try:
-            _, filename, peer_port, seek = msg.split()
-        except ValueError:
-            print('CTCP DCC ACCEPT unexpected format in request')
-            return
-
-        mqtt.pub('Notifications/irc', f'Resuming transfer of {filename}')
-        peer_address = self.dccpeers[peer_nick]
-        self.dccfiles[peer_address].file.resume(int(seek))
-        self._safe_dcc_connect(peer_address, peer_port)
-
-    def _safe_dcc_connect(self, peer_address, peer_port) -> None:
-        filename = self.dccfiles[peer_address].file.filename
-        print(f'fetching {filename}')
-        try:
-            self.dccfiles[peer_address].dcc.connect(peer_address, int(peer_port))
-        except irc.client.DCCConnectionError as e:
-            print(f'Transfer of {filename} failed due to {e}')
-            mqtt.pub('Notifications/irc', f'Transfer of {filename} failed due to {e}')
+from typing import Any, Tuple
+from ircbot import IRCBot, ServerConnection, Event, DCCConnectionError  # type: ignore
+from mqtt import MQTT, Client, MQTTMessage  # type: ignore
 
 
 class File():
-    def __init__(self, filename: str, size: int) -> None:
+    def __init__(self, filename: str) -> None:
         self.fileh = None
-        self.received = False
-        self.size = size
+        self.md5 = hashlib.md5()
         self.filename = pathlib.Path('/data') / pathlib.Path(filename).name
 
         self.received_bytes = 0
         if self.filename.exists():
             self.received_bytes = self.filename.stat().st_size
-            if self.received_bytes >= self.size:
-                self.received = True
 
     def resume(self, offset: int) -> None:
         if not self.fileh:
@@ -187,84 +34,120 @@ class File():
         if not self.fileh:
             self.fileh = self.filename.open('wb')
         self.fileh.write(data)
+        self.md5.update(data)
         self.received_bytes = self.received_bytes + len(data)
-        if self.size <= 4294967295:
-            format = '!I'  # 4-bit big-endian unsigned int
-        else:
-            format = '!Q'  # 8-bit big-endian unsigned long long int
-        return struct.pack(format, self.received_bytes)
+        return self.received_bytes
 
     def close(self) -> None:
         self.fileh.close()
+        self.fileh = None
 
 
-class MQTT():
-    def __init__(self, host: str, port: int = 1883, user=None, password=None, keepalive=60, client_id=None):
-        self.client_id = client_id
-        self.host = host
-        self.port = port
-        self.keepalive = keepalive
-        self.client = paho.mqtt.client.Client(client_id)
+class Transfers():
+    # transfer schema {'name', 'size', 'src', 'md5', 'ip', 'port', 'file', 'connection'}
+    def __init__(self) -> None:
+        self.transfers = []
 
-        self.client.tls_set()
-        self.client.tls_insecure_set(True)
+    def upsert_transfer(self, name: str, **kwargs) -> dict:
+        transfer = self.find_transfer_by_name(name)
+        if not transfer:
+            transfer = {'name': name, 'file': File(name)}
+            self.transfers.append(transfer)
+        return self.update_transfer(transfer, **kwargs)
 
-        if user or password:
-            self.client.username_pw_set(user, password)
+    def update_transfer(self, transfer: dict, **kwargs) -> dict:
+        for key, value in kwargs.items():
+            if key in ['port', 'size', 'startat']:
+                value = int(value)
+            transfer[key] = value
+        return transfer
 
-        self.client.on_message = self.on_message
-        self.client.on_connect = self.on_connect
-        self.client.on_disconnect = self.on_disconnect
+    def start(self, transfer: dict) -> bool:
+        try:
+            transfer['connection'].connect(transfer['ip'], transfer['port'])
+        except DCCConnectionError as e:
+            print('ERROR', str(e))
+            return False
+        transfer['file'].resume(transfer.get('startat', 0))
+        return True
 
-    def on_message(self, client: Client, userdata: Any, msg: MQTTMessage):
-        print(f'Msg: {msg.topic} {str(msg.qos)} {str(msg.payload)}')
-        if msg.topic == 'Commands/ALL' and msg.payload == b'check-in':
-            self.pub('Notifications/check-in-reply', self.client_id, qos=1)
+    def write_and_reply(self, transfer: dict, content: bytes) -> None:
+        if transfer:
+            transfer['file'].write(content)
+            if transfer.get('size', 0) <= 4294967295:
+                format = '!I'  # 4-bit big-endian unsigned int
+            else:
+                format = '!Q'  # 8-bit big-endian unsigned long long int
+            transfer['connection'].send_bytes(struct.pack(format, transfer['file'].received_bytes))
 
-    def on_connect(self, client: Client, userdata: Any, flags: dict, rc):
-        print(f'Connected to {client._host}:{client._port} code {rc}')
-        self.sub(None, 'Commands/ALL', qos=1)
-        self.pub('Notifications/startup', f'{self.client_id} connect at {time.time()}', qos=1)
-
-    def on_disconnect(self, client: Client, userdata: Any, rc):
-        print(f'Disconnected from {client._host}:{client._port} code {rc}')
-
-    def pub(self, topic: str, message: str, retain: bool = False, qos: int = 0):
-        self.client.publish(topic, message, qos)
-
-    def sub(self, callback: Callable, topic: str, qos: int = 0):
-        if callback:
-            self.client.message_callback_add(topic, callback)
-        self.client.subscribe(topic, qos)
-
-    def listen(self, blocking: bool = False):
-        """
-        This is the main loop for a client waiting for messages
-        """
-        self.client.connect(self.host, self.port, self.keepalive)
-        if blocking:
-            print('Starting blocking MQTT loop', flush=True)
-            self.client.loop_forever()
+    def close(self, transfer: dict) -> Tuple[str, float]:
+        transfer['file'].close()
+        transfer['end_md5'] = transfer['file'].md5.hexdigest()
+        if transfer['size'] > 0:
+            transfer['pct_complete'] = transfer['file'].received_bytes / transfer['size'] * 100
         else:
-            print('Starting nonblocking MQTT thread', flush=True)
-            self.client.loop_start()
+            transfer['pct_complete'] = float('nan')
+        return transfer
+
+    def find_transfer_by_name(self, name: str) -> dict:
+        return self._find_transfer_by_x('name', name)
+
+    def find_transfer_by_src(self, src: str) -> dict:
+        return self._find_transfer_by_x('src', src)
+
+    def find_transfer_by_ip(self, ip: str) -> dict:
+        return self._find_transfer_by_x('ip', ip)
+
+    def _find_transfer_by_x(self, field: str, value: str) -> dict:
+        for transfer in self.transfers:
+            if transfer.get(field) == value:
+                return transfer
+        return {}
 
 
 class Bot():
-    def start(self):
-        mqtt.listen()
-        mqtt.sub(self.bridge, 'Commands/IRC')
+    def __init__(self) -> None:
 
-        client.connect()
-        while True:
-            client.reactor.process_once()
-            time.sleep(0.01)
+        self.mqtt = MQTT(os.getenv('IRC_MQTT_BROKER'),
+                         int(os.getenv('IRC_MQTT_PORT')),
+                         os.getenv('IRC_MQTT_USER'),
+                         os.getenv('IRC_MQTT_PASS'),
+                         use_ssl=True,
+                         client_id='mqtt-irc-bridge'
+                         )
+        self.irc = IRCBot(os.getenv('IRC_SERVER'),
+                          int(os.getenv('IRC_PORT')),
+                          os.getenv('IRC_NICKNAME'),
+                          os.getenv('IRC_NICKSERV_PASS'),
+                          os.getenv('IRC_CHANNELS').split(';')
+                          )
+        self.transfers = Transfers()
+        self.debug_file = open('/data/unparsed.log', 'at')
+        self.watchlist = os.getenv('IRC_WATCHLIST').split(';')
 
-    def stop(self):
-        client.connection.disconnect('bye')
-        print('Cleanly exited')
+        self.callbacks = [
+            ('ctcp', 'Process CTCP Messages', self.handle_ctcp),
+            ('dccmsg', 'Dispatch DCC messages to appropriate object', self.handle_dcc_msg),
+            ('dcc_disconnect', 'Close DCC chat states', self.handle_dcc_disconnect),
+            ('ping', 'Ping Debug', self.debug_print),
+            ('privmsg', 'Private Message Debug', self.debug_print),
+            ('privnotice', 'Record interesting values from private notices', self.handle_watchlist),
+            ('pubmsg', 'Record interesting public messages from watchlisted channels', self.handle_watchlist),
+            ('pubnotice', 'Public Notice Debug', self.debug_print)
+        ]
+        for eventtype, _, callback in self.callbacks:
+            self.irc.sub(callback, eventtype)
 
-    def bridge(self, mosq, obj, msg):
+    def start(self) -> None:
+        self.mqtt.listen()
+        self.mqtt.sub(self.mqtt_bridge, 'Commands/IRC')
+
+        self.irc.start()
+
+    def stop(self) -> None:
+        self.irc.stop()
+
+    def mqtt_bridge(self, client: Client, userdata: Any, msg: MQTTMessage) -> None:
         """MQTT Callback"""
         try:
             payload = json.loads(msg.payload)
@@ -272,25 +155,87 @@ class Bot():
             payload = {'type': 'broken'}
         print('MQTT', payload)
         if payload.get('type') == 'privmsg':
-            client.connection.privmsg(payload['target'], payload['msg'])
+            self.irc.privmsg(payload['target'], payload['msg'])
         else:
-            mqtt.pub('Notifications/errors', 'Unknown IRC Command')
+            self.mqtt.pub('Notifications/errors', 'Unknown IRC Command')
+
+    def handle_watchlist(self, connection: ServerConnection, event: Event) -> None:
+        """IRC Callback"""
+        if event.target in self.watchlist:
+            extract = re.match(r'.{4,16} +\d+x \[([^\]]+)\] (.*)', event.arguments[0])
+            if extract:
+                src = event.source.nick
+                meta = extract.group(1)
+                name = extract.group(2)
+                self.mqtt.pub('IRC/watchlist', json.dumps({'src': src, 'meta': meta, 'name': name}))
+            else:
+                self.debug('WATCHLIST', event)
+        elif event.target == connection.nickname:
+            extract = re.match(r'.{21,30}\"([^\"]+)\".{3,15}\w{3}:([^\]]+)', event.arguments[0])
+            if extract:
+                name = extract.group(1)
+                md5 = extract.group(2)
+                self.transfers.upsert_transfer(name=name, md5=md5)
+
+    def handle_ctcp(self, connection: ServerConnection, event: Event) -> None:
+        """IRC Callback"""
+        if event.arguments[0] == 'DCC':
+            self.handle_dcc(connection, event)
+        else:
+            print('CTCP', event)
+
+    def handle_dcc(self, connection: ServerConnection, event: Event) -> None:
+        src = event.source.nick
+        cmd, name, v1, v2, size = (event.arguments[1].split() + [0])[:5]
+        if cmd == 'SEND':   # DCC SEND filename ip port size
+            ip = ".".join(map(str, struct.unpack('BBBB', struct.pack('>L', int(v1)))))
+            metadata = {'name': name, 'src': src, 'ip': ip, 'port': v2, 'size': size, 'connection': self.irc.dcc()}
+        elif cmd == 'ACCEPT':  # DCC ACCEPT filename port position
+            metadata = {'name': name, 'src': src, 'port': v1, 'startat': v2}
+        else:
+            return
+
+        transfer = self.transfers.upsert_transfer(**metadata)
+
+        if transfer['file'].received_bytes == transfer['size']:
+            msg = (f'{name} already transferred, not sending')
+        elif cmd == 'SEND' and transfer['file'].received_bytes > 16384:
+            msg = (f'Requesting resume of {name}')
+            # DCC RESUME filename port position
+            # subtract 16384 bytes à la mirc as this seems to be one block
+            self.irc.ctcp('DCC', src, f"RESUME {name} {transfer['port']} {transfer['file'].received_bytes - 16384}")
+        else:
+            if self.transfers.start(transfer):
+                msg = f"Started transfer of {name} from {src} at {metadata.get('startat', 0)}B"
+        self.mqtt.pub('Notifications/irc', msg, verbose=True)
+
+    def handle_dcc_msg(self, connection: ServerConnection, event: Event) -> None:
+        """IRC Callback"""
+        self.transfers.write_and_reply(self.transfers.find_transfer_by_ip(event.source), event.arguments[0])
+
+    def handle_dcc_disconnect(self, connection: ServerConnection, event: Event) -> None:
+        """IRC Callback"""
+        transfer = self.transfers.close(self.transfers.find_transfer_by_ip(event.source))
+        print('TRANSFER', transfer, flush=True)
+
+        valid = 'verified' if transfer.get('md5', '') == transfer.get('end_md5') else 'unverified'
+        msg = f"Received {valid} transfer of {transfer['pct_complete']:0.2f}% of file {transfer['name']}"
+        self.mqtt.pub('Notifications/irc', msg, verbose=True)
+
+    def debug(self, *msg) -> None:
+        print(*msg, file=self.debug_file, flush=True)
+
+    def debug_print(self, connection: ServerConnection, event: Event) -> None:
+        """IRC Callback"""
+        print(event, flush=True)
 
 
 if __name__ == "__main__":
-    irc.client.ServerConnection.buffer_class = buffer.LenientDecodingLineBuffer
-    ge = os.getenv
-    mqtt = MQTT(ge('MQTT_BROKER'), int(ge('MQTT_PORT')), ge('MQTT_USER'), ge('MQTT_PASS'), client_id='mqtt-irc-bridge')
-    client = IRC(ge('IRC_SERVER'), int(ge('IRC_PORT')), ge('NICKNAME'))
     bot = Bot()
     try:
         bot.start()
-    except irc.client.ServerConnectionError as x:
-        print(x)
-    except KeyboardInterrupt:
-        bot.stop()
-    except Exception as e:
+    except (KeyboardInterrupt, Exception) as e:
         print(e, flush=True)
-        bot.stop()
-        mqtt.pub('Notifications/irc', f'Error: {e}')
         raise
+    finally:
+        bot.stop()
